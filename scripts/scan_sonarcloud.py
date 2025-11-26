14096d0ff51ec490ae32d11d19a00e9fe4699a48
Emma <120013670+7MMA7@users.noreply.github.com>
2025-11-26 16:08:24 -0500

    fix: chunk file > 90

--- scripts/scan_sonarcloud.py
+++ scripts/scan_sonarcloud.py
@@ -169,7 +169,7 @@ if os.path.exists(WORKDIR):
     shutil.rmtree(WORKDIR)
 os.makedirs(WORKDIR)
 
-input_file = "chunk_90.jsonl"
+input_file = "chunk_9000.jsonl"
 if not os.path.exists(input_file):
     print(f"{input_file} not found")
     exit(1)
