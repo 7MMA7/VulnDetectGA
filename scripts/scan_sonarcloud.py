27091e44bc531cb4b8b568f33b8357b3be30dbce
Emma <120013670+7MMA7@users.noreply.github.com>
2025-11-26 15:37:18 -0500

    change chunk file

--- scripts/scan_sonarcloud.py
+++ scripts/scan_sonarcloud.py
@@ -169,7 +169,7 @@ if os.path.exists(WORKDIR):
     shutil.rmtree(WORKDIR)
 os.makedirs(WORKDIR)
 
-input_file = "chunk_80.jsonl"
+input_file = "chunk_90.jsonl"
 if not os.path.exists(input_file):
     print(f"{input_file} not found")
     exit(1)
